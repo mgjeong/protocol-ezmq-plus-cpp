dce22b8f43622ea7891cfc61380fb9a347b4e0cd
bg.chun <bg.chun@samsung.com>
2018-02-01 16:40:36 +0900

    build.sh for armhf
    Signed-off-by: bg.chun <bg.chun@samsung.com>

---
+++ build_arm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PROJECT_ROOT=$(pwd)
+DEP_ROOT=$(pwd)/dependencies
+EZMQ_TARGET_ARCH="$(uname -m)"
+
+if [ -d "./dependencies" ] ; then
+    echo "dependencies folder exist"
+else
+    mkdir dependencies
+fi
+
+#build ezmq-cpp
+cd ./dependencies
+#clone ezmq-protocol-cpp
+git clone git@github.sec.samsung.net:RS7-EdgeComputing/protocol-ezmq-cpp.git
+cd ./protocol-ezmq-cpp
+#./build.sh --with_dependencies=true --target_arch=x86
+#./build.sh --target_arch=x86_64
+echo "build protocol-ezmq-cpp"
+scons TARGET_OS=linux TARGET_ARCH=armhf
+echo "done"
+
+#build ezmq-plus-cpp
+cd $PROJECT_ROOT
+#scons TARGET_OS=linux TARGET_ARCH=x86_64
+echo "build protocol-ezmq-plus-cpp"
+scons TARGET_OS=linux TARGET_ARCH=armhf
+echo "done"
