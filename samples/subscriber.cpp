5e58a2bcd3f8bd1ffb5d3a2a3387b673477d1673
bg.chun <bg.chun@samsung.com>
2018-02-08 20:07:21 +0900

    dummy subscriber for dockerize test
    Signed-off-by: bg.chun <bg.chun@samsung.com>

---
+++ samples/subscriber.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <EZMQXPublisher.h>
+#include <EZMQXException.h>
+int main()
+{
+
+    int dummy = 3;
+    void *payload = (void *)&dummy;
+    try
+    {
+      // error callback
+      EZMQX::PubErrCb errCb = [](std::shared_ptr<EZMQX::Publisher> publisher, EZMQX::ErrorCode errCode){std::cout << "called" << std::endl;};
+      
+      // create publisher with test topic
+      std::shared_ptr<EZMQX::Publisher> publisher = EZMQX::Publisher::getPublisher("/topic", "test topic", errCb);
+      
+      // publish
+      publisher->publish(payload);
+
+      // condition check
+      if (publisher->isTerminated())
+      {
+          publisher->terminate();
+      }
+
+      // occur exception
+      if (publisher->isTerminated())
+      {
+          publisher->terminate();
+      }
+    }
+    catch(EZMQX::Exception e)
+    {
+        // catch terminated exception
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout << "done" << std::endl;
+}
