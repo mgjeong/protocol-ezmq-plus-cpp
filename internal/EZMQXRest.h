b4e4c2b0e30cfb76a5a071c849098ee9c11564f5
bg.chun <bg.chun@samsung.com>
2018-02-22 13:37:01 +0900

    Define Rest class
    Signed-off-by: bg.chun <bg.chun@samsung.com>

---
+++ internal/EZMQXRest.h
@@ -0,0 +1,27 @@
+#ifndef EZMQ_X_REST_H
+#define EZMQ_X_REST_H
+
+#include <string>
+#include <curl/curl.h>
+
+namespace EZMQX {
+
+class SimpleRest
+{
+private:
+    CURL *curl;
+    // make noncopyable
+    SimpleRest(const SimpleRest&) = delete;
+    SimpleRest& operator = (const SimpleRest&) = delete;
+public:
+    SimpleRest();
+    ~SimpleRest();
+    std::string Get(std::string url);
+    std::string Put(std::string url);
+    std::string Post(std::string url);
+    std::string Delete(std::string url);
+};
+
+} // namespace EZMQX
+
+#endif //EZMQ_X_REST_H
--- src/EZMQXContext.cpp
+++ src/EZMQXContext.cpp
@@ -1,7 +1,13 @@
 #include <EZMQXContext.h>
 #include <EZMQXErrorCode.h>
 #include <EZMQXException.h>
-#include <curl.h>
+#include <EZMQXRest.h>
+#include <iostream>
+#include <fstream>
+#include <streambuf>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
 
 std::shared_ptr<EZMQX::Context> EZMQX::Context::_instance;
 
@@ -38,16 +44,74 @@ void EZMQX::Context::initialize()
         {
 
             // initialize resource
-            CURL *curl;
-            CURLcode res;
-            curl_global_init(CURL_GLOBAL_DEFAULT);
-            curl = curl_easy_init();
 
             // parse host & broker addr
+            std::string nodeInfo;
+            {
+                EZMQX::SimpleRest rest;
+                nodeInfo = rest.Get("http://10.113.77.245:48098/api/v1/management/device/configuration");
+            }
+
+            // std::cout << nodeInfo <<std::endl;
+
+            if (nodeInfo.empty())
+            {
+                // do something
+            }
+
+            Json::Value root;
+            Json::Reader reader;
+            if (!reader.parse(nodeInfo, root))
+            {
+                std::cout << "json parse fail" << std::endl;
+            }
+            else
+            {
+                Json::Value props = root["properties"];
+
+                for (Json::Value::ArrayIndex i = 0; i < props.size(); i ++)
+                {
+                    if (!this->remoteAddr.empty() && !this->hostAddr.empty())
+                    {
+                        break;
+                    }
+
+                    if (props[i].isMember("name"))
+                    {
+
+                         if (props[i]["name"].asString() == "anchoraddress")
+                         {
+                            this->remoteAddr = props[i]["value"].asString();
+
+                         }
+                         else if (props[i]["name"].asString() == "nodeaddress")
+                         {
+                            this->hostAddr = props[i]["value"].asString();
+                         }
+                    }
+                }
+            }
+
 
             // get hostname
+            std::ifstream _file("/etc/hostname");
+            std::string _hostname((std::istreambuf_iterator<char>(_file)), std::istreambuf_iterator<char>());
+            // check last is '\n'
+            if (_hostname.back() == '\n')
+            {
+                _hostname = _hostname.substr(0, _hostname.size()-1);
+            }
+            this->hostname = _hostname;
+
+
+            std::cout << this->remoteAddr <<std::endl;
+            std::cout << this->hostAddr <<std::endl;
+            std::cout << this->hostname <<std::endl;
 
             // parse port mapping table
+
+            // get apps info from node
+                // get app detail info
         }
 
         initialized.store(true);
