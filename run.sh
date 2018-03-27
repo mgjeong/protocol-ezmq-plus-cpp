2fc9074c80a885df3291a9c27390351d38a64b6a
bg.chun <bg.chun@samsung.com>
2018-03-27 20:32:42 +0900

    update build scripts
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- run.sh
+++ run.sh
@@ -7,21 +7,35 @@ cp -r libs/* /usr/local/lib/
 ldconfig
 
 #excute sample
-if [ "publisher" = $1 ]; then
-    echo "start publisher with topic: $2"
-    ./publisher $2
-elif [ "subscriber" = $1 ]; then
-    echo "start subscriber with topic $2"
-    ./AmlSubscriber $2
-else
-    echo "Wrong arguments!!!"
-    echo "Example"
-    echo " publisher topic"
-    echo " subscriber topic"
-fi
+if [ "loop" = $1 ]; then
     while true
     do
         echo "Entered infinite loop for test"
         sleep 1
     done
-    
+elif [ "publisher" = $1 ]; then
+    echo "start publisher with topic: $2"
+    ./publisher $2
+elif [ "subscriber" = $1 ]; then
+    if [ "aml" = $2 ]; then
+        echo "start subscriber with topic: $3"
+        ./AmlSubscriber $3
+    elif [ "xml" = $2 ]; then
+        echo "start subscriber with topic: $3"
+        ./XmlSubscriber $3
+    else
+        echo "Wrong arguments!!!"
+        echo "Example"
+        echo " loop"
+        echo " publisher topic"
+        echo " subscriber aml topic"
+        echo " subscriber xml topic"
+    fi
+else
+    echo "Wrong arguments!!!"
+    echo "Example"
+    echo " loop"
+    echo " publisher topic"
+    echo " subscriber aml topic"
+    echo " subscriber xml topic"
+fi
