37ddc465f04485c6a6a23b95c078e8a5160c3df9
bg.chun <bg.chun@samsung.com>
2018-04-09 15:15:06 +0900

    create TopicDiscovery sample
    Signed-off-by: bg.chun <bg.chun@samsung.com>

---
+++ samples/TopicDiscovery.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <EZMQXConfig.h>
+#include <EZMQXTopicDiscovery.h>
+#include <EZMQXException.h>
+
+int main()
+{
+    std::shared_ptr<EZMQX::Config> config(new EZMQX::Config(EZMQX::FullFeature));
+    // std::shared_ptr<EZMQX::Config> config(new EZMQX::Config(EZMQX::StandAlone));
+    //config->setHostInfo("TestPublisher", "10.113.77.33");
+    // config->setTnsInfo("10.113.66.234:48323");
+    std::shared_ptr<EZMQX::TopicDiscovery> discovery(new EZMQX::TopicDiscovery());
+    std::list<EZMQX::Topic> result;
+    while (1)
+    {
+        std::string topic;
+        std::cout<<"Enter topic ex) /test/"<<std::endl;
+        std::cin>>topic;
+
+        try
+        {
+            if (!result.empty())
+            {
+                result.clear();
+            }
+            result = discovery->query(topic);
+
+            for (auto itr = result.begin(); itr != result.end(); itr++)
+            {
+                std::cout<<"========================================"<<std::endl;
+                EZMQX::Topic tmp = *itr;
+                std::cout<<"Topic: "<<tmp.getTopic()<<std::endl;
+                std::cout<<"Endpoint: "<<tmp.getEndpoint().toString()<<std::endl;
+                std::cout<<"Schema: "<<tmp.getSchema()<<std::endl;
+            }
+            std::cout<<"========================================"<<std::endl;
+        }
+        catch (const EZMQX::Exception& e)
+        {
+            std::cout<<"Catch Exception: "<<e.what()<<std::endl;
+        }
+        catch (...)
+        {
+            std::cout<<"Unknown exception throwed"<<std::endl;
+        }
+    }
+
+    return 0;
+}
