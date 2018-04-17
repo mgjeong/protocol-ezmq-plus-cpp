811e4f7e08846e6f05bd0bb9c7bf7bb07a9cc550
bg.chun <bg.chun@samsung.com>
2018-04-17 16:27:03 +0900

    update topicdiscovery unittest
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- unittests/ezmqx_topicdiscovery_test.cpp
+++ unittests/ezmqx_topicdiscovery_test.cpp
@@ -1,8 +1,71 @@
+#include <gtest.h>
+#include <gmock.h>
 #include <iostream>
 #include <EZMQXConfig.h>
 #include <EZMQXException.h>
+#include <UnitTestHelper.h>
+#include <string>
 
-int main()
+using testing::Action;
+using testing::ActionInterface;
+using testing::Assign;
+using testing::ByMove;
+using testing::ByRef;
+using testing::DefaultValue;
+using testing::DoDefault;
+using testing::IgnoreResult;
+using testing::Invoke;
+using testing::InvokeWithoutArgs;
+using testing::MakePolymorphicAction;
+using testing::Ne;
+using testing::PolymorphicAction;
+using testing::Return;
+using testing::ReturnNull;
+using testing::ReturnRef;
+using testing::ReturnRefOfCopy;
+using testing::SetArgPointee;
+using testing::SetArgumentPointee;
+using testing::Unused;
+using testing::_;
+using testing::AtLeast;
+using testing::get;
+using testing::internal::BuiltInDefaultValue;
+using testing::internal::Int64;
+using testing::internal::UInt64;
+using testing::make_tuple;
+using testing::tuple;
+using testing::tuple_element;
+
+TEST_F(DiscoveryTest, WithTns)
 {
-    return 0;
+    EXPECT_CALL(mock, verifyTopic(_, _))
+    .Times(1);
+
+    try
+    {
+        setDummyTns();
+        mock.query("/abc");
+    }
+    catch(...)
+    {
+
+    }
+    
+}
+
+TEST_F(DiscoveryTest, WithTns2)
+{
+    EXPECT_CALL(mock, verifyTopic(_, _))
+    .Times(1);
+
+    try
+    {
+        setDummyTns();
+        mock.query("/abc");
+    }
+    catch(...)
+    {
+
+    }
+    
 }
