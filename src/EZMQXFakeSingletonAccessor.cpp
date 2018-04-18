a15c129297f3e0fce815c44da97b943d452a2bcc
bg.chun <bg.chun@samsung.com>
2018-04-18 21:43:07 +0900

    update fake accesser for mocking
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- src/EZMQXFakeSingletonAccessor.cpp
+++ src/EZMQXFakeSingletonAccessor.cpp
@@ -4,21 +4,22 @@
 static const std::string FAKE_HOST_NAME = "localhost";
 static const std::string FAKE_HOST_ADDR = "localhost";
 static const std::string FAKE_REMOTE_ADDR = "localhost";
+static const int LOCAL_PORT_START = 4000;
+static const int LOCAL_PORT_MAX = 100;
 
 void EZMQX::FakeSingletonAccessor::setFake()
 {
-    try
+    EZMQX::Context* ctx = EZMQX::Context::getInstance();
+    ctx->setHostInfo(FAKE_HOST_NAME, FAKE_HOST_ADDR);
+    ctx->setTnsInfo(FAKE_REMOTE_ADDR);
+
+    for (int i = LOCAL_PORT_START; i <= LOCAL_PORT_MAX; i++)
     {
-        EZMQX::Context* ctx = EZMQX::Context::getInstance();
-        ctx->setStandAloneMode(true);
-        ctx->setHostInfo(FAKE_HOST_NAME, FAKE_HOST_ADDR);
-        ctx->setTnsInfo(FAKE_REMOTE_ADDR);
-    }
-    catch(...)
-    {
-        // do nothing
+        // set dummy
+        ctx->ports[i] = i;
     }
 
-    
+    ctx->initialized.store(true);
+
     return;
 }
