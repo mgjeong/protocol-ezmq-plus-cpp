cf8cddbdfa967e7c3d4b9cbbe2495883cac2e23e
bg.chun <bg.chun@samsung.com>
2018-05-14 18:34:27 +0900

    update sample publisher duration to 100ms for stress test
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- samples/publisher.cpp
+++ samples/publisher.cpp
@@ -158,7 +158,7 @@ int main()
           publisherC->publish(amlObj);
           std::cout << "Publish!!!" << std::endl;
           printAMLObject(amlObj);
-          std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
       }
 
     }
