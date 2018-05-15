b4f1d0bec85aa4edaad48a88b935eae8d0085b49
bg.chun <bg.chun@samsung.com>
2018-05-15 21:18:42 +0900

    update subscriber unittest
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- unittests/UnitTestHelper.h
+++ unittests/UnitTestHelper.h
@@ -2,6 +2,7 @@
 #include <EZMQXConfig.h>
 #include <EZMQXTopicDiscovery.h>
 #include <EZMQXAMLSubscriber.h>
+#include <EZMQXXMLSubscriber.h>
 #include <EZMQXAmlPublisher.h>
 #include <EZMQXException.h>
 #include <EZMQXFakeSingletonAccessor.h>
@@ -9,6 +10,223 @@
 #include <gtest.h>
 #include <gmock.h>
 #include <string>
+
+void printAMLData(AML::AMLData amlData, int depth)
+{
+    std::string indent;
+    for (int i = 0; i < depth; i++) indent += "    ";
+
+    std::cout << indent << "{" << std::endl;
+
+    std::vector<std::string> keys = amlData.getKeys();
+    for (std::string key : keys)
+    {
+        std::cout << indent << "    \"" << key << "\" : ";
+
+        AML::AMLValueType type = amlData.getValueType(key);
+        if (AML::AMLValueType::String == type)
+        {
+            std::string valStr = amlData.getValueToStr(key);
+            std::cout << valStr;
+        }
+        else if (AML::AMLValueType::StringArray == type)
+        {
+            std::vector<std::string> valStrArr = amlData.getValueToStrArr(key);
+            std::cout << "[";
+            for (std::string val : valStrArr)
+            {
+                std::cout << val;
+                if (val != valStrArr.back()) std::cout << ", ";
+            }
+            std::cout << "]";
+        }
+        else if (AML::AMLValueType::AMLData == type)
+        {
+            AML::AMLData valAMLData = amlData.getValueToAMLData(key);
+            std::cout << std::endl;
+            printAMLData(valAMLData, depth + 1);
+        }
+
+        if (key != keys.back()) std::cout << ",";
+        std::cout << std::endl;
+    }
+    std::cout << indent << "}";
+}
+
+void printAMLObject(AML::AMLObject amlObj)
+{
+    std::cout << "{" << std::endl;
+    std::cout << "    \"device\" : " << amlObj.getDeviceId() << "," << std::endl;
+    std::cout << "    \"timestamp\" : " << amlObj.getTimeStamp() << "," << std::endl;
+    std::cout << "    \"id\" : " << amlObj.getId() << "," << std::endl;
+
+    std::vector<std::string> dataNames = amlObj.getDataNames();
+
+    for (std::string n : dataNames)
+    {
+        AML::AMLData data = amlObj.getData(n);
+
+        std::cout << "    \"" << n << "\" : " << std::endl;
+        printAMLData(data, 1);
+        if (n != dataNames.back()) std::cout << "," << std::endl;
+    }
+
+    std::cout << "\n}" << std::endl;
+}
+
+
+class SubStandAloneTest : public testing::Test
+{
+protected:
+    EZMQX::Config *config;
+    std::string dummyId;
+    EZMQX::AmlPublisher* pub;
+    virtual void SetUp()
+    {
+        config = EZMQX::Config::getInstance();
+        config->startStandAloneMode(false, "localhost");
+        std::list<std::string> amlPath(1, "sample_data_model.aml");
+        std::list<std::string> amlId = config->addAmlModel(amlPath);
+        dummyId = amlId.front();
+
+        pub = EZMQX::AmlPublisher::getPublisher("/T/A", EZMQX::AmlModelId, dummyId, 4000);
+    }
+
+    virtual void TearDown()
+    {
+        config->reset();
+
+        if (pub)
+        {
+            delete pub;
+            pub = nullptr;
+        }
+    }
+
+    std::string getDummyId()
+    {
+        return dummyId;
+    }
+
+    EZMQX::Topic getPositiveTestTopic()
+    {
+        return EZMQX::Topic("/T/A", getDummyId(), EZMQX::Endpoint("localhost", 4000));
+    }
+
+    std::list<EZMQX::Topic> getPositiveTestTopics()
+    {
+        std::list<EZMQX::Topic> dummy;
+        dummy.push_back(EZMQX::Topic("/T/A", getDummyId(), EZMQX::Endpoint("localhost", 4000)));
+        dummy.push_back(EZMQX::Topic("/T/B", getDummyId(), EZMQX::Endpoint("localhost", 4001)));
+        dummy.push_back(EZMQX::Topic("/T/C", getDummyId(), EZMQX::Endpoint("localhost", 4002)));
+        return dummy;
+    }
+
+    EZMQX::Topic getNegativeTestTopic()
+    {
+        return EZMQX::Topic("/T/A", "ssss", EZMQX::Endpoint("localhost", 4000));
+    }
+
+    std::list<EZMQX::Topic> getNegativeTestTopics()
+    {
+        std::list<EZMQX::Topic> dummy;
+        dummy.push_back(EZMQX::Topic("/T/A", "ssss", EZMQX::Endpoint("localhost", 4000)));
+        dummy.push_back(EZMQX::Topic("/T/B", "ssss", EZMQX::Endpoint("localhost", 4001)));
+        dummy.push_back(EZMQX::Topic("/T/C", "ssss", EZMQX::Endpoint("localhost", 4002)));
+        return dummy;
+    }
+
+    void destoryPub()
+    {
+        if (pub)
+        {
+            delete pub;
+            pub = nullptr;
+        }
+    }
+
+    void pubTest()
+    {
+        // create AMLObject
+            std::string deviceId = "GTC001";
+            std::string timeStamp = "123456789";
+
+            AML::AMLObject amlObj(deviceId, timeStamp);
+
+            // create "Model" data
+            AML::AMLData model;
+            model.setValue("ctname", "Model_107.113.97.248");
+            model.setValue("con", "SR-P7-970");
+
+            // create "Sample" data
+            AML::AMLData axis;
+            axis.setValue("x", "20");
+            axis.setValue("y", "110");
+            axis.setValue("z", "80");
+
+            AML::AMLData info;
+            info.setValue("id", "f437da3b");
+            info.setValue("axis", axis);
+
+            std::vector<std::string> appendix;
+            appendix.push_back("52303");
+            appendix.push_back("935");
+            appendix.push_back("1442");
+
+            AML::AMLData sample;
+            sample.setValue("info", info);
+            sample.setValue("appendix", appendix);
+
+            // Add Datas to AMLObject
+            amlObj.addData("Model", model);
+            amlObj.addData("Sample", sample);
+            if (pub)
+            {
+                pub->publish(amlObj);
+            }
+
+    }
+};
+
+class SubDockerTest : public testing::Test
+{
+protected:
+    EZMQX::Config *config;
+    std::string dummyId;
+    virtual void SetUp()
+    {
+        EZMQX::FakeSingletonAccessor::setFake();
+        config = EZMQX::Config::getInstance();
+        config->startDockerMode();
+        std::list<std::string> amlPath(1, "sample_data_model.aml");
+        std::list<std::string> amlId = config->addAmlModel(amlPath);
+        dummyId = amlId.front();
+    }
+
+    virtual void TearDown()
+    {
+        config->reset();
+    }
+
+    std::string getDummyId()
+    {
+        return dummyId;
+    }
+
+    EZMQX::Topic getTestTopic()
+    {
+        return EZMQX::Topic("/T/A", getDummyId(), EZMQX::Endpoint("localhost", 4000));
+    }
+
+    std::list<EZMQX::Topic> getDummyTopics()
+    {
+        std::list<EZMQX::Topic> dummy;
+        dummy.push_back(EZMQX::Topic("/TEST/A", getDummyId(), EZMQX::Endpoint("localhost", 4000)));
+        dummy.push_back(EZMQX::Topic("/TEST/B", getDummyId(), EZMQX::Endpoint("localhost", 4001)));
+        dummy.push_back(EZMQX::Topic("/TEST/C", getDummyId(), EZMQX::Endpoint("localhost", 4002)));
+        return dummy;
+    }
+};
 
 class MockAmlSubscriber : public EZMQX::AmlSubscriber
 {
--- unittests/ezmqx_subscriber_test.cpp
+++ unittests/ezmqx_subscriber_test.cpp
@@ -37,51 +37,226 @@ using testing::tuple;
 using testing::tuple_element;
 using testing::SetArgReferee;
 
-TEST_F(DockerAmlSubscriber, ExpectThrow)
-{
+// TEST_F(DockerAmlSubscriber, ExpectThrow)
+// {
 
-    EXPECT_CALL(*mock, verifyTopics(_, _, _))
-    .Times(2);
+//     EXPECT_CALL(*mock, verifyTopics(_, _, _))
+//     .Times(2);
 
-    std::string topic = "/TEST";
+//     std::string topic = "/TEST";
 
-    EXPECT_THROW(mock->initialize(topic, true), EZMQX::Exception);
-    EXPECT_THROW(mock->initialize(topic, false), EZMQX::Exception);
-}
+//     EXPECT_THROW(mock->initialize(topic, true), EZMQX::Exception);
+//     EXPECT_THROW(mock->initialize(topic, false), EZMQX::Exception);
+// }
 
-TEST_F(DockerAmlSubscriber, ExpectThreeDummies)
-{
+// TEST_F(DockerAmlSubscriber, ExpectThreeDummies)
+// {
 
-    EXPECT_CALL(*mock, verifyTopics(_, _, _))
-    .WillOnce(SetArgReferee<1>(getDummyTopics()));
+//     EXPECT_CALL(*mock, verifyTopics(_, _, _))
+//     .WillOnce(SetArgReferee<1>(getDummyTopics()));
 
-    EXPECT_CALL(*mock, getSession(_))
-    .Times(3);
+//     EXPECT_CALL(*mock, getSession(_))
+//     .Times(3);
 
-    std::string topic = "/TEST";
+//     std::string topic = "/TEST";
 
-    mock->initialize(topic, true);
-    std::list<EZMQX::Topic> result = mock->getTopics();
+//     mock->initialize(topic, true);
+//     std::list<EZMQX::Topic> result = mock->getTopics();
 
-    EXPECT_TRUE(result.size() == 3);
+//     EXPECT_TRUE(result.size() == 3);
 
-    EZMQX::Topic first = result.front();
-    EXPECT_TRUE(first.getName().compare("/TEST/A") == 0);
-    EXPECT_TRUE(first.getDatamodel().compare(getDummyId()) == 0);
-    EXPECT_TRUE(first.getEndpoint().toString().compare("localhost:4000") == 0);
-    result.pop_front();
+//     EZMQX::Topic first = result.front();
+//     EXPECT_TRUE(first.getName().compare("/TEST/A") == 0);
+//     EXPECT_TRUE(first.getDatamodel().compare(getDummyId()) == 0);
+//     EXPECT_TRUE(first.getEndpoint().toString().compare("localhost:4000") == 0);
+//     result.pop_front();
 
-    EZMQX::Topic second = result.front();
-    EXPECT_TRUE(second.getName().compare("/TEST/B") == 0);
-    EXPECT_TRUE(second.getDatamodel().compare(getDummyId()) == 0);
-    EXPECT_TRUE(second.getEndpoint().toString().compare("localhost:4001") == 0);
-    result.pop_front();
+//     EZMQX::Topic second = result.front();
+//     EXPECT_TRUE(second.getName().compare("/TEST/B") == 0);
+//     EXPECT_TRUE(second.getDatamodel().compare(getDummyId()) == 0);
+//     EXPECT_TRUE(second.getEndpoint().toString().compare("localhost:4001") == 0);
+//     result.pop_front();
 
-    EZMQX::Topic third = result.front();
-    EXPECT_TRUE(third.getName().compare("/TEST/C") == 0);
-    EXPECT_TRUE(third.getDatamodel().compare(getDummyId()) == 0);
-    EXPECT_TRUE(third.getEndpoint().toString().compare("localhost:4002") == 0);
-    result.pop_front();
+//     EZMQX::Topic third = result.front();
+//     EXPECT_TRUE(third.getName().compare("/TEST/C") == 0);
+//     EXPECT_TRUE(third.getDatamodel().compare(getDummyId()) == 0);
+//     EXPECT_TRUE(third.getEndpoint().toString().compare("localhost:4002") == 0);
+//     result.pop_front();
 
     
+// }
+
+
+//Aml Test
+TEST_F(SubDockerTest, AmlSubFailTest)
+{
+    EZMQX::AmlSubCb subCb = [](std::string topic, const AML::AMLObject& payload){std::cout << "subCb called" << std::endl << "topic: " << topic << std::endl; printAMLObject(payload);};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::AmlSubscriber* sub1 = nullptr;
+    EZMQX::AmlSubscriber* sub2 = nullptr;
+
+
+    EXPECT_THROW(sub1 = EZMQX::AmlSubscriber::getSubscriber("T/A", true, subCb, errCb), EZMQX::Exception);
+    EXPECT_THROW(sub2 = EZMQX::AmlSubscriber::getSubscriber("T/A", false, subCb, errCb), EZMQX::Exception);
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, AmlSubFailTest2)
+{
+    EZMQX::AmlSubCb subCb = [](std::string topic, const AML::AMLObject& payload){std::cout << "subCb called" << std::endl << "topic: " << topic << std::endl; printAMLObject(payload);};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::AmlSubscriber* sub1 = nullptr;
+    EZMQX::AmlSubscriber* sub2 = nullptr;
+
+    EXPECT_THROW(sub1 = EZMQX::AmlSubscriber::getSubscriber(getNegativeTestTopic(), subCb, errCb), EZMQX::Exception);
+    EXPECT_THROW(sub2 = EZMQX::AmlSubscriber::getSubscriber(getNegativeTestTopics(), subCb, errCb), EZMQX::Exception);
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, AmlSubSuccessTest1)
+{
+    EZMQX::AmlSubCb subCb = [](std::string topic, const AML::AMLObject& payload){std::cout << "subCb called" << std::endl << "topic: " << topic << std::endl; printAMLObject(payload);};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::AmlSubscriber* sub1 = EZMQX::AmlSubscriber::getSubscriber(getPositiveTestTopic(), subCb, errCb);
+    EZMQX::AmlSubscriber* sub2 = EZMQX::AmlSubscriber::getSubscriber(getPositiveTestTopics(), subCb, errCb);
+
+    if (sub1)
+    {
+        sub1->isTerminated();
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, AmlSubActualTest1)
+{
+    EZMQX::AmlSubCb subCb = [](std::string topic, const AML::AMLObject& payload){std::cout << "subCb called" << std::endl << "topic: " << topic << std::endl; printAMLObject(payload);};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::AmlSubscriber* sub1 = EZMQX::AmlSubscriber::getSubscriber(getPositiveTestTopic(), subCb, errCb);
+
+    pubTest();
+    pubTest();
+    pubTest();
+
+    destoryPub();
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
+}
+
+//Xml Test
+
+TEST_F(SubDockerTest, XmlSubFailTest)
+{
+    EZMQX::XmlSubCb subCb = [](std::string topic, const std::string& payload){std::cout << "subCb called" << std::endl << "topic: " <<  topic << std::endl << payload << std::endl; std::cout << payload << std::endl;};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::XmlSubscriber* sub1 = nullptr;
+    EZMQX::XmlSubscriber* sub2 = nullptr;
+
+
+    EXPECT_THROW(sub1 = EZMQX::XmlSubscriber::getSubscriber("T/A", true, subCb, errCb), EZMQX::Exception);
+    EXPECT_THROW(sub2 = EZMQX::XmlSubscriber::getSubscriber("T/A", false, subCb, errCb), EZMQX::Exception);
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, XmlSubFailTest2)
+{
+    EZMQX::XmlSubCb subCb = [](std::string topic, const std::string& payload){std::cout << "subCb called" << std::endl << "topic: " <<  topic << std::endl << payload << std::endl; std::cout << payload << std::endl;};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::XmlSubscriber* sub1 = nullptr;
+    EZMQX::XmlSubscriber* sub2 = nullptr;
+
+    EXPECT_THROW(sub1 = EZMQX::XmlSubscriber::getSubscriber(getNegativeTestTopic(), subCb, errCb), EZMQX::Exception);
+    EXPECT_THROW(sub2 = EZMQX::XmlSubscriber::getSubscriber(getNegativeTestTopics(), subCb, errCb), EZMQX::Exception);
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, XmlSubSuccessTest1)
+{
+    EZMQX::XmlSubCb subCb = [](std::string topic, const std::string& payload){std::cout << "subCb called" << std::endl << "topic: " <<  topic << std::endl << payload << std::endl; std::cout << payload << std::endl;};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::XmlSubscriber* sub1 = EZMQX::XmlSubscriber::getSubscriber(getPositiveTestTopic(), subCb, errCb);
+    EZMQX::XmlSubscriber* sub2 = EZMQX::XmlSubscriber::getSubscriber(getPositiveTestTopics(), subCb, errCb);
+
+    if (sub1)
+    {
+        sub1->isTerminated();
+        delete sub1;
+    }
+
+    if (sub2)
+    {
+        delete sub2;
+    }
+}
+
+TEST_F(SubStandAloneTest, XmlSubActualTest1)
+{
+    EZMQX::XmlSubCb subCb = [](std::string topic, const std::string& payload){std::cout << "subCb called" << std::endl << "topic: " <<  topic << std::endl << payload << std::endl; std::cout << payload << std::endl;};
+    EZMQX::SubErrCb errCb = [](std::string topic, EZMQX::ErrorCode errCode){std::cout << "errCb called" << std::endl << "topic: " <<  topic << std::endl << "err: " << errCode << std::endl;};
+
+    EZMQX::XmlSubscriber* sub1 = EZMQX::XmlSubscriber::getSubscriber(getPositiveTestTopic(), subCb, errCb);
+    sub1->getTopics();
+
+    pubTest();
+    pubTest();
+    pubTest();
+
+    destoryPub();
+
+    if (sub1)
+    {
+        delete sub1;
+    }
+
 }
