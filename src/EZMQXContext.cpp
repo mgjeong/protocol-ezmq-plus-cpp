5fbc90795e7113e9bd2160b18750b5af84fc9459
bg.chun <bg.chun@samsung.com>
2018-07-31 15:29:39 +0900

    add logs for tns
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- src/EZMQXContext.cpp
+++ src/EZMQXContext.cpp
@@ -479,6 +479,8 @@ void EZMQX::Context::initialize(const std::string& tnsConfPathRef)
                         tnsAddr = HTTP + ipaddr + COLLON + TNS_KNOWN_PORT;
                     }
 
+                    EZMQX_LOG_V(DEBUG, TAG, "%s tns server address is %s", __func__, this->tnsAddr.c_str());
+
                     this->tnsEnabled = true;
 
                     reverseProxy.store(reverseProxyEnabled);
